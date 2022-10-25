ee189deec0d289523fb78fe64181419eb4976d37
ISKANDAR HAKIMI ZULKIPPLI <iskandarhkimi43@gmail.com>
2022-10-26 00:45:05 +0800

    UI for Candidate List & MPP Alumni
    - Added UI for View candidate list (admin)
    - Added UI for View MPP Alumni (voter, candidate, admin)
    - Added UI for Edit MPP Alumni Page (Admin)
    
    *Note
    - Edit function for MPP Alumni is basically the same as edit About Us page, can edit about MPP Alumni paragraph.

--- resources/views/admin/candidate/candidatelist/index.blade.php
+++ resources/views/admin/candidate/candidatelist/index.blade.php
@@ -1,11 +1,65 @@
 {{-- ADMIN CANDIDATE LIST CONTENT --}}
 @extends('layouts.admin')
 @extends('layouts.app')
+@section('content2')
+    {{-- first four column --}}
+<!DOCTYPE html>
+<html>
+<head>
 
-@section('content')
-  <div class="card">
-    <div class="card-body">
-      <h1>Admin Candidate List Content</h1>
+</head>
+<body>
+    <div class="row" style="padding-top: 1.5rem; padding-left: 1.5rem;">
+      
+      <div class="col-md-12">
+        <div class="card">
+          <div class="card-header">
+            <h2 class="card-title" style="text-align: center;">LIST OF CANDIDATES</h2>
+          </div>
+          <div class="card-body" style="text-align: center">
+                
+                    <div style="padding: 15px">
+                        <h4>GENERAL SEAT</h4> 
+                        <p style="font-size: 1.0rem;">1.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    2.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    3.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)</p>
+                    </div>
+
+                    <div style="padding: 15px">
+                        <h4>AZMAN HASHIM INTERNATIONAL BUSINESS SCHOOL (AHIBS)</h4> 
+                        <p style="font-size: 1.0rem;">1.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)<br>
+                                                    2.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)<br>
+                                                    3.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)</p>
+                    </div>
+
+                    <div style="padding: 15px">
+                        <h4>RAZAK FACULTY OF TECHNOLOGY AND INFORMATICS (FTIR)</h4> 
+                        <p style="font-size: 1.0rem;">1.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)<br>
+                                                    2.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)<br>
+                                                    3.  ISKANDAR HAKIMI ZULKIPPLI (2 DDWD)</p>
+                    </div>
+
+                    <div style="padding: 15px">
+                        <h4>MALAYSIA-JAPAN INTERNATIONAL INSTITUTE OF TECHNOLOGY (MJIIT)</h4> 
+                        <p style="font-size: 1.0rem;">1.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    2.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    3.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)</p>
+                    </div>
+
+                    <div style="padding: 15px">
+                        <h4>SCHOOL OF PROFESSIONAL AND CONTINUING EDUCATION (SPACE)</h4> 
+                        <p style="font-size: 1.0rem;">1.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    2.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)<br>
+                                                    3.  ISKANDAR HAKIMI ZULKIPPLI (3 DDWD)</p>
+                    </div>
+                  
+                </div>
+              
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
+    
+</body>
+</html>
 @endsection
